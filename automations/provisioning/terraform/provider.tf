fc0df3bd047f01e9f63f01a257ff302adc8a4d6e
Daniel Fazekas <daniel.fazekas@gmail.com>
2022-05-15 16:35:43 +0200

    add another version

--- automations/provisioning/terraform/provider.tf
+++ automations/provisioning/terraform/provider.tf
@@ -9,7 +9,7 @@ terraform {
     required_providers {
         proxmox = {
             source = "telmate/proxmox"
-            version = "2.8.0
+            version = "2.7.4"
         }
     }
 }
@@ -32,7 +32,6 @@ provider "proxmox" {
     pm_api_token_id = var.proxmox_api_token_id
     pm_api_token_secret = var.proxmox_api_token_secret
     pm_timeout = 1800
-
     # (Optional) Skip TLS Verification
     # pm_tls_insecure = true
 
