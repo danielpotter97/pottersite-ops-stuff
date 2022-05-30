23078677a2e97c2a31b37c284b99f0c67b960b04
Daniel Fazekas <daniel.fazekas@gmail.com>
2022-05-30 21:55:33 +0200

    add boot order

--- automations/provisioning/terraform/clone-vm.tf
+++ automations/provisioning/terraform/clone-vm.tf
@@ -23,12 +23,13 @@ resource "proxmox_vm_qemu" "pottersite-docker01" {
     cores = 2
     sockets = 1
     cpu = "host"    
-    boot = "order=scsi0;net0"
+  
     # VM Memory Settings
     memory = 8192
 
     scsihw  = "virtio-scsi-pci"
-
+    bootdisk = "scsi0"
+    boot = "order=scsi0;net0"
     network {
         bridge = "vmbr0"
         model  = "virtio"
