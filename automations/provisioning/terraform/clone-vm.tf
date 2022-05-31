4b6d64a387ea22c279aa6d1cefe262961ea84f91
Daniel Fazekas <daniel.fazekas@gmail.com>
2022-05-31 12:07:28 +0200

    modify

--- automations/provisioning/terraform/clone-vm.tf
+++ automations/provisioning/terraform/clone-vm.tf
@@ -27,9 +27,14 @@ resource "proxmox_vm_qemu" "pottersite-docker01" {
     # VM Memory Settings
     memory = 8192
 
-    scsihw  = "virtio-scsi-pci"
+    ipconfig0 = "ip=192.168.0.22/24,gw=192.168.0.1"
+
+    nameserver = "192.168.0.1"
+
+    scsihw  = "virtio-scsi-pci"ó
     bootdisk = "virtio0"
     boot = "order=virtio0;net0"
+
     network {
         bridge = "vmbr0"
         model  = "virtio"
@@ -38,9 +43,13 @@ resource "proxmox_vm_qemu" "pottersite-docker01" {
     # VM Cloud-Init Settings
     os_type = "cloud-init"
     
-    ipconfig0 = "ip=192.168.0.22/24,gw=192.168.0.1"
-
-    nameserver = "192.168.0.1"
+  
  
- 
+     # (Optional) Default User
+    ciuser = "potteradmins"
+    
+    # (Optional) Add your SSH KEY
+    sshkeys = <<EOF
+    ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC1PLAFEKbjTZFsu/L+A/Stai93GT47gvrrI6TlhWKDroXQSIUtBMKlwTS/FRCHDxrSY/rNa1+/i5t1tnjxdzQa1nCGWlTJXeiGtYYfhSy+lU8KAqUJhCwyJfa+WGv4mNbx6QyohuEFoTVzyCbfvEnvTJhgwMF+VD9HGczVQHnF58syfzz6pjPuDJ6RE16O6SQCnSWv5SPEyxh/qjz/MzG6id8IHQPqkM1mV64ILVQ09vslK5e0mH85nEnOmaeJK6Vrd79/MfUvRgQVVcE2gzMhYQTUgESivqqVme55rDK1Y5wXyKEGekxJuiPPdnr12HYPEVJR9YCYIXqsjG4CNARtqdyZOOX7Bt/NzVDDE44SH3Vvfu7jEt48y6ekgPJoz+6HNWz8C4+lcKdU+LzDgQAamaxDQhf4P+JrDTUNGyBnizIo4+cREwDoSerMmaP56f9SMTctUVmC8QjzjXAYo+xBZ2CwprHLOV41O3HH6kj7fYYWOrBrnkulAjanxDRBFeM= potteradmin@jenkins01.pottersite.local
+    EOF
 }
